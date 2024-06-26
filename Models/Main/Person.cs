4b142a1dd86b2b2d60753b6af933e325774b11f5
UinLan <kimlanlongtuyen@gmail.com>
2024-06-26 10:25:02 +0700

    back-end

---
+++ Models/Main/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoSucKhoe.Models.Main;
+
+public partial class Person
+{
+    public int Id { get; set; }
+
+    public string? Hoten { get; set; }
+
+    public string? Username { get; set; }
+
+    public int? Phone { get; set; }
+
+    public string? AddressUser { get; set; }
+
+    public string? Pass { get; set; }
+
+    public string? RoleUser { get; set; }
+
+    public virtual ICollection<Lichtiem> Lichtiems { get; set; } = new List<Lichtiem>();
+
+    public virtual ICollection<PhieuDinhKy> PhieuDinhKies { get; set; } = new List<PhieuDinhKy>();
+
+    public virtual ICollection<PhieuSucKhoe> PhieuSucKhoes { get; set; } = new List<PhieuSucKhoe>();
+}
---
+++ Models/Main/PhieuSucKhoe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoSucKhoe.Models.Main;
+
+public partial class PhieuSucKhoe
+{
+    public int Id { get; set; }
+
+    public int? PersonId { get; set; }
+
+    public DateTime? NgayKham { get; set; }
+
+    public string? Diadiem { get; set; }
+
+    public string? TenBenh { get; set; }
+
+    public double? ChiPhi { get; set; }
+
+    public string? LuuY { get; set; }
+
+    public string? LoaiBenh { get; set; }
+
+    public string? TrieuChung { get; set; }
+
+    public string? Kham { get; set; }
+
+    public string? DinhBenh { get; set; }
+
+    public string? MoTaToaThuoc { get; set; }
+
+    public string? HinhToaThuoc { get; set; }
+
+    public virtual Person? Person { get; set; }
+}
